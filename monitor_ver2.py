209ea062380d2319c527a3c3f8bf94342356dfb9
Fabio <38531334+BianchiFabio@users.noreply.github.com>
2020-05-05 18:15:12 +0200

    Update monitor_ver2.py

--- monitor_ver2.py
+++ monitor_ver2.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# -------------------------------------------------------------------------
+
 dati = pd.read_csv ('200505_Dati_sensori_aria.csv', sep=";",header = 0)
 # Carico i dati dal .csv nel pd dati ATTENZIONE AL SEPARATORE DEI DATI DEL CSV: A VOLTE E' , A VOLTE E' ;
 print (dati.head(2))
 
+# -------------------------------------------------------------------------
+
 datiPM10 = dati [dati.IdSensore == 6951] # filtro le righe che contengono i dati IdSensore selezionato
 datiPM2_5 = dati [dati.IdSensore == 30158] # filtro le righe che contengono i dati IdSensore selezionato
+
+# -------------------------------------------------------------------------
 
 # PM 10
 fig, ax = plt.subplots()
@@ -21,6 +27,7 @@ ax.grid(which='major', linestyle='-', linewidth='1.5', color='green')
 ax.grid(which='minor', linestyle=':', linewidth='0.5', color='red')
 
 # -------------------------------------------------------------------------
+
 # PM 2.5
 fig2, ax = plt.subplots()
 # aggiungo una colonna che contiene le date contenute in Data e le trasforma in type datetime
@@ -35,15 +42,15 @@ ax.grid(which='minor', linestyle=':', linewidth='0.5', color='red')
 
 plt.show()
 
-# ---------------------------
+# -------------------------------------------------------------------------
 
-#::::::::::::::::::::::::::::::::::::::::::::::::
 # Fabio Bianchi
 # 12/01/2020
 # Monitoraggio qualita' aria
 #
 # Disclaimer: tutti i dati riportati devono essere riscontrati dall'utilizzatore finale e NON hanno carattere di ufficialità
 #             Il creatore del codice declina ogni responsabilità dalla diffusione illecita o non confutata di tali dati.
+
 
 #:::::::::::::::::::::::::::::::::::::::::::::::: CENTRALINE
 # Mappa delle stazioni
@@ -66,12 +73,12 @@ def centraline ():
     Disclaimer: tutti i dati riportati devono essere riscontrati dall'utilizzatore finale e NON hanno carattere di ufficialità
                 Il creatore del codice declina ogni responsabilità dalla diffusione illecita o non confutata di tali dati.                
                 
-    #                                                                CENTRALINE                                                                           #      
+    #                                                      CENTRALINE                                                                           #      
 
     #::::::::::::::::::::::::::::::::::::::::::::::::::::#           #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#    
-    #                STAZIONE ANALIZZATA                 #           #                          STAZIONE ANALIZZATA                       #
-    # Nome stazione              Brescia - via Turati    #           # Nome stazione                 Brescia - Broletto                  #
-    # Idstazione                 652                     #           # Idstazione                            649                                 #
+    #                STAZIONE ANALIZZATA                 #           #                      STAZIONE ANALIZZATA                       #
+    # Nome stazione  Brescia - via Turati                #           # Nome stazione         Brescia - Broletto                       #
+    # Idstazione           652                           #           # Idstazione                   649                               #
     # ---------------------------------------------------#           # ---------------------------------------------------------------#
     # IdSensore | NomeTipoSensore          | UnitaMisura #           # IdSensore | NomeTipoSensore          | DataStop  | UnitaMisura #
     # ----------|--------------------------|-------------#           # ----------|--------------------------|-----------+-------------#   
